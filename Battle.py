2ad4976cfb4821d8186fab0d8f3c8269940c4e04
NathanDeodato <ndeodato42@outlook.com>
2021-02-16 12:43:22 -0300

    Update Battle.py

--- Battle.py
+++ Battle.py
@@ -1,30 +1,34 @@
 # Libs
 from Battles import Battles
 from random import randint
+from time import sleep
 
 # Colors
-ciano = "\033[1;96m"
+cinza = "\033[1;90m"
 vermelho = "\033[1;31m"
-branco = "\033[1;32m"
 branco = "\033[1;97m"
 
 # Start
-print(ciano + "=" * 150)
+print(cinza + "=" * 150)
 print(branco + "-- Battle-Game --".center(150))
-print(ciano + "=" * 150)
+print(cinza + "=" * 150)
 
 print(branco + "Esse é um jogo de batalha, com uma mecanica de batalhas por rodada.")
 print(branco + "Está preparado para jogar contra mim? Sou sua máquina. Hehehe")
 print(branco + "VAMOS LÁ!")
 
+sleep(1)
+
 
 while True:
     try:
-        print(ciano + "=" * 150)
+        print(cinza + "=" * 150)
         
         print(branco + "Selecione sua classe de guerreiro")
     
-        print(ciano + "-" * 150)
+        print(cinza + "-" * 150)
+
+        sleep(1)
 
         print(branco + "[ 1 ] Warrior")
         print("[ 2 ] Paladin")
@@ -32,7 +36,9 @@ while True:
         print("[ 4 ] Wizard")
         print("[ 5 ] Assassin")
 
-        print(ciano + "=" * 150)
+        sleep(1)
+
+        print(cinza + "=" * 150)
 
         player = int(input(branco + "Selecione a classe: "))
         while player != 1 and player != 2 and player != 3 and player != 4 and player != 5:
@@ -43,22 +49,28 @@ while True:
 
         Battles(player, machine)
 
-        print(ciano + "=" * 150)
+        print(cinza + "=" * 150)
         
+        sleep(1)
+
         print(branco + "[ 1 ] Sim")
         print(branco + "[ 2 ] Não")
 
-        print(ciano + "-" * 150)
+        print(cinza + "-" * 150)
+
+        sleep(1)
 
         go = int(input(branco + "Continue? "))
         
         if go == 2:
-            print(ciano + "=" * 150)
+            sleep(1)
+
+            print(cinza + "=" * 150)
             print(branco + "-- Battle-Close --".center(150))
-            print(ciano + "=" * 150)
+            print(cinza + "=" * 150)
 
             break
 
     except Exception as erro:
-        print(vermelho + "Erro: {erro}")
+        print(vermelho + f"Erro: {erro}")
 
